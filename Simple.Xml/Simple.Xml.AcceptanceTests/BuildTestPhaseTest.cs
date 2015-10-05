b415535c4c3616c2f92478e4916491e029d575ae
baks <baks@users.noreply.github.com>
2015-10-05 14:43:11 +0200

    First Xunit test
    It's purpose is to test whether travis runs tests.

---
+++ Simple.Xml/Simple.Xml.AcceptanceTests/BuildTestPhaseTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Simple.Xml.AcceptanceTests
+{
+    public class BuildTestPhaseTest
+    {
+        [Fact]
+        public void ShouldTestFailedShowUpOnTravis()
+        {
+            Assert.False(true);
+        }
+    }
+}
