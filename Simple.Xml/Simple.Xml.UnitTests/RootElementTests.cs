1ca169c7055e2e9a10adbb37c5fb6a4f58be73af
baks <baks@users.noreply.github.com>
2015-11-08 09:14:35 +0100

    RootElement tests

---
+++ Simple.Xml/Simple.Xml.UnitTests/RootElementTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NSubstitute;
+using Ploeh.AutoFixture;
+using Xunit;
+
+namespace Simple.Xml.UnitTests
+{
+    public class RootElementTests
+    {
+        private readonly RootElement sut = new RootElement();
+        private readonly IDownwardElementVisitor downwardVisitor = Substitute.For<IDownwardElementVisitor>();
+        private readonly IUpwardElementVisitor upwardVisitor = Substitute.For<IUpwardElementVisitor>();
+
+        [Theory, AutoSubstituteData]
+        public void PassesVisitorIntoAllChildren(IEnumerable<IElement> children)
+        {
+            foreach (var child in children)
+            {
+                sut.AddChild(child);
+            }
+            sut.Accept(downwardVisitor);
+
+            AssertPassesDownwardVisitorToAllChildren(children);
+        }
+
+        [Fact]
+        public void DoesNotVisitUpwardVisitor()
+        {
+            sut.Accept(upwardVisitor);
+            
+            upwardVisitor.DidNotReceive().Visit(AName, AParent, AnElementsEnumerable);   
+        }
+
+        private void AssertPassesDownwardVisitorToAllChildren(IEnumerable<IElement> children)
+        {
+            foreach (var child in children)
+            {
+                child.Received(1).Accept(downwardVisitor);
+            }
+        }
+
+        private static string AName => Arg.Any<string>();
+
+        private static IEnumerable<IElement> AnElementsEnumerable => Arg.Any<IEnumerable<IElement>>();
+
+        private static IElement AParent => Arg.Any<IElement>();
+    }
+}
