e4ba86be33d193823d6e4d59a6b17a71718607c5
baks <baks@users.noreply.github.com>
2015-11-10 09:58:26 +0100

    Dynamic Backward/Forward xml creation tests

---
+++ Simple.Xml/Simple.Xml.Dynamic.UnitTests/DynamicBackwardXmlCreatorTests.cs
@@ -0,0 +1,20 @@
+﻿using NSubstitute;
+using Simple.Xml.Structure;
+using Xunit;
+
+namespace Simple.Xml.Dynamic.UnitTests
+{
+    public class DynamicBackwardXmlCreatorTests
+    {
+        private readonly IElement element = Substitute.For<IElement>();
+        private readonly DynamicBackwardXmlCreator sut = new DynamicBackwardXmlCreator();
+
+        [Fact]
+        public void PassesUpwardVisitorToElement()
+        {
+            sut.Visit(element);
+
+            element.Received(1).Accept(Arg.Any<IUpwardElementVisitor>());
+        }
+    }
+}
---
+++ Simple.Xml/Simple.Xml.Dynamic.UnitTests/DynamicForwardXmlStringCreatorTests.cs
@@ -0,0 +1,20 @@
+﻿using NSubstitute;
+using Simple.Xml.Structure;
+using Xunit;
+
+namespace Simple.Xml.Dynamic.UnitTests
+{
+    public class DynamicForwardXmlStringCreatorTests
+    {
+        private readonly IElement element = Substitute.For<IElement>();
+        private readonly DynamicForwardXmlStringCreator sut = new DynamicForwardXmlStringCreator();
+
+        [Fact]
+        public void PassesDownwardVisitorToElement()
+        {
+            sut.Visit(element);
+
+            element.Received(1).Accept(Arg.Any<IDownwardElementVisitor>());
+        }
+    }
+}
